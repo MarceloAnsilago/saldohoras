5f53bca947827a9baeb1caf7546fbcce2a7d7e35
MarceloAnsilago <celosmg1@gmail.com>
2026-09-16 09:06:40 -0400

    trabalhando as radio

--- saldohoras.py
+++ saldohoras.py
@@ -810,25 +810,31 @@ with aba_mapa_plantao:
         with st.container(border=True):
             st.date_input("Data inicial", key="data_inicial_mapa_plantao_col1")
             st.date_input("Data final", key="data_final_mapa_plantao_col1")
-            st.radio(
-                "Trabalha",
-                ["Sim", "Nao"],
-                horizontal=True,
-                key="trabalha_mapa_plantao_col1",
-            )
 
     with mapa_col2:
         with st.container(border=True):
             st.date_input("Data inicial", key="data_inicial_mapa_plantao_col2")
             st.date_input("Data final", key="data_final_mapa_plantao_col2")
-            st.radio(
-                "Trabalha",
-                ["Sim", "Nao"],
-                horizontal=True,
-                key="trabalha_mapa_plantao_col2",
-            )
 
-    st.selectbox("Equipe", EQUIPES, key="equipe_mapa_plantao")
+    opcoes_col1, opcoes_col2 = st.columns(2)
+
+    with opcoes_col1:
+        st.radio(
+            "Trabalha periodo 1",
+            ["Sim", "Nao"],
+            horizontal=True,
+            key="trabalha_mapa_plantao_col1",
+        )
+        st.radio(
+            "Trabalha periodo 2",
+            ["Sim", "Nao"],
+            horizontal=True,
+            key="trabalha_mapa_plantao_col2",
+        )
+
+    with opcoes_col2:
+        st.selectbox("Equipe", EQUIPES, key="equipe_mapa_plantao")
+
     st.button(
         "Adicionar",
         type="primary",
